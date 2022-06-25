c0dc773525239ef679cd8e869d6b107b637f1a55
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-06-25 22:52:29 +0530

    Create Diagonal Difference.py

---
+++ HACKERRANK/Diagonal Difference.py
@@ -0,0 +1,100 @@
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+# For example, the square matrix  is shown below:
+
+# 1 2 3
+# 4 5 6
+# 9 8 9  
+# The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+# Function description
+
+# Complete the  function in the editor below.
+
+# diagonalDifference takes the following parameter:
+
+# int arr[n][m]: an array of integers
+# Return
+
+# int: the absolute diagonal difference
+# Input Format
+
+# The first line contains a single integer, , the number of rows and columns in the square matrix .
+# Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+# Constraints
+
+# Output Format
+
+# Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+# Sample Input
+
+# 3
+# 11 2 4
+# 4 5 6
+# 10 8 -12
+# Sample Output
+
+# 15
+# Explanation
+
+# The primary diagonal is:
+
+# 11
+#    5
+#      -12
+# Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+# The secondary diagonal is:
+
+#      4
+#    5
+# 10
+# Sum across the secondary diagonal: 4 + 5 + 10 = 19
+# Difference: |4 - 19| = 15
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    pri_d = [];
+    pri_m = 0;
+    sec_d = [];
+    sec_m = n - 1;
+    for i in arr:
+        pri_d.append(i[pri_m]);
+        sec_d.append(i[sec_m]);
+        pri_m = pri_m + 1;
+        sec_m = sec_m - 1;
+    return abs(sum(pri_d)-sum(sec_d))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
