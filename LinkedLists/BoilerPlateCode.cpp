59a70bdc6ee97a600c47e3e07bffb6cd5fc9f216
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-07-02 16:01:45 +0530

    Create BoilerPlateCode.cpp

---
+++ LinkedLists/BoilerPlateCode.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    /* data */
+    int data;
+    struct Node* next;
+};
+
+struct Node* insert_end(struct Node* head,int data)
+{
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=data;
+    temp->next=NULL;
+    if(head==NULL)
+    {
+        head=temp;
+        return head;
+    }
+    struct Node* ptr=head;
+    while(ptr->next!=NULL)
+    {
+        ptr=ptr->next;
+    }
+    ptr->next=temp;
+    return head;
+}
+
+
+void printLL(struct Node* n)
+{
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+int main()
+{
+    struct Node* head=NULL;
+    //head=inser_end(head,value);
+}
