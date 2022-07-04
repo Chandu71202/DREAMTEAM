7dd38a44a58dc67f52a342a26f02ab38a7201327
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-07-04 23:58:01 +0530

    Create heightOfaBinaryTree.cpp

---
+++ Tree/heightOfaBinaryTree.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        if(root==NULL)
+        return -1;
+        int h;
+        int lh=height(root->left);
+        int rh=height(root->right);
+        if(lh>rh)
+        h=1+lh;
+        else
+        h=1+rh;
+        return h;
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
+
+
+// INPUT : 
+// 7
+// 3 2 5 1 4 6 7
+// OUTPUT:
+// 3
