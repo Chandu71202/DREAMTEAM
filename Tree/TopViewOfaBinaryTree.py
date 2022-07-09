9969829ea7913ca555ecc8b7475390b802a51b36
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-07-09 22:06:20 +0530

    Update TopViewOfaBinaryTree.py

--- Tree/TopViewOfaBinaryTree.py
+++ Tree/TopViewOfaBinaryTree.py
@@ -1,84 +1,63 @@
-from sys import stdin, setrecursionlimit
-from queue import Queue
-setrecursionlimit(10**7)
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
 
-# Following is the TreeNode class structure:
-class BinaryTreeNode:
-    def __init__(self, data):
-        self.val = data
-        self.left = None
-        self.right = None
+    def __str__(self):
+        return str(self.info) 
 
-def getTopView(root):
-    # Write your code here.
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def topView(root):
+    #Write your code here
     q=[]
     m=dict()
-    hd=0
-    root.hd=hd
-    q.root()
+    level=0
+    root.level=level
+    q.append(root)
     while(q):
         root=q[0]
-        hd=root.hd
-        if hd not in m:
-            m[hd]=root.data
-        if(root.left):
-            root.left.hd=hd-1
+        level=root.level
+        if level not in m:
+            m[level]=root.info
+        if root.left:
+            root.left.level=level-1
             q.append(root.left)
-        if(root.right):
-            root.right.hd=hd+1
+        if root.right:
+            root.right.level=level+1
             q.append(root.right)
-        q.pop()
-        for i in sorted(m):
-            print(m[i],end=" ")
-        
-# Taking input.
-def takeInput():
-
-    arr = list(map(int, stdin.readline().strip().split(" ")))
-
-    rootData = arr[0]
-
-    n = len(arr)
-
-    if(rootData == -1):
-        return None
-
-    root = BinaryTreeNode(rootData)
-    q = Queue()
-    q.put(root)
-    index = 1
-    while(q.qsize() > 0):
-        currentNode = q.get()
-
-        leftChild = arr[index]
-
-        if(leftChild != -1):
-            leftNode = BinaryTreeNode(leftChild)
-            currentNode.left = leftNode
-            q.put(leftNode)
-
-        index += 1
-        rightChild = arr[index]
-
-        if(rightChild != -1):
-            rightNode = BinaryTreeNode(rightChild)
-            currentNode .right = rightNode
-            q.put(rightNode)
-
-        index += 1
-
-    return root
-
-# Printing the tree nodes.
-def printAns(ans):
-    for x in ans:
-        print(x, end=" ")
-    print()
-
-
-# Main.
-T = int(input().strip())
-for i in range(T):
-    root = takeInput()
-    ans = getTopView(root)
-    printAns(ans)
+        q.pop(0)
+    for i in sorted(m):
+        print(m[i],end=" ")            
